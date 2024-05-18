b92ed1a7524b9046d1156da744c0af4d10f90ca0
Vinayak Suthar <sutharvinayak28@gmail.com>
2024-05-18 10:44:01 +0530

    feat: add settings panel to update node

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,21 +1,93 @@
+import { ArrowLeftIcon } from "@heroicons/react/16/solid";
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/outline";
+import { useEffect, useRef, useState } from "react";
+import { useOnSelectionChange, useReactFlow } from "reactflow";
 
 export default function Sidebar() {
+  const { setNodes } = useReactFlow();
+  const [selectedNode, setSelectedNode] = useState(null);
+  const inputRef = useRef();
+  const { id, data } = selectedNode || {};
+
+  useOnSelectionChange({
+    onChange: ({ nodes }) => {
+      if (nodes.length > 0) setSelectedNode(nodes[0]);
+      else setSelectedNode(null);
+    },
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    const updatedNode = {
+      ...selectedNode,
+      data: {
+        ...selectedNode.data,
+        [name]: value,
+      },
+    };
+
+    setSelectedNode(updatedNode);
+
+    setNodes((nodes) =>
+      nodes.map((node) => {
+        if (node.id === id) {
+          return updatedNode;
+        }
+        return node;
+      })
+    );
+  }
+
   function handleDragStart(event) {
     event.dataTransfer.setData("application/reactflow", "custom");
     event.dataTransfer.effectAllowed = "move";
   }
 
+  useEffect(() => {
+    if (inputRef.current) inputRef.current.focus();
+  }, [selectedNode]);
+
   return (
-    <aside className="border-l-2 w-[400px] grid grid-cols-2 gap-3 p-3">
-      <div
-        onDragStart={handleDragStart}
-        draggable
-        className={`border cursor-grab bg-white border-blue-400 rounded h-[100px] flex flex-col justify-center items-center text-blue-400`}
-      >
-        <ChatBubbleOvalLeftEllipsisIcon className="size-6" />
-        Message
-      </div>
+    <aside className="border-l-2 w-[400px]">
+      {!selectedNode && (
+        <div className="grid grid-cols-2 gap-3 p-3">
+          <div
+            onDragStart={handleDragStart}
+            draggable
+            className={`border cursor-grab bg-white border-blue-400 rounded h-[100px] flex flex-col justify-center items-center text-blue-400`}
+          >
+            <ChatBubbleOvalLeftEllipsisIcon className="size-6" />
+            Message
+          </div>
+        </div>
+      )}
+      {selectedNode && (
+        <>
+          <div className="flex items-center border-b-2 p-2">
+            <button>
+              <ArrowLeftIcon className="size-5" />
+            </button>
+            <p className="flex-1 text-center">Message</p>
+          </div>
+          <div className="px-3 py-6 border-b-2">
+            <div className="flex flex-col gap-y-2 ">
+              <label htmlFor={`label-${id}`} className="text-gray-600 text-sm">
+                Text
+              </label>
+              <textarea
+                name="label"
+                ref={inputRef}
+                autoFocus
+                id={`label-${id}`}
+                value={data.label}
+                onChange={handleChange}
+                className="border border-slate-300 rounded p-2 text-sm outline-slate-400"
+              />
+            </div>
+          </div>
+        </>
+      )}
     </aside>
   );
 }
